39698057faa67b71c7ba0e28c2657c5407bc4237
DongsubShin <pottersd2@naver.com>
2022-11-16 18:34:13 +0900

    sdf

--- deploy/setup.sh
+++ deploy/setup.sh
@@ -3,9 +3,9 @@
 set -e
 
 # TODO: Set to URL of git repo.
-PROJECT_GIT_URL='https://github.com/DongsubShin/potential_backend.git'
+PROJECT_GIT_URL='https://github.com/DongsubShin/w88.git'
 
-PROJECT_BASE_PATH='/usr/local/apps/potential'
+PROJECT_BASE_PATH='/usr/local/apps/w88'
 
 echo "Installing dependencies..."
 apt-get update
--- deploy/update.sh
+++ deploy/update.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-PROJECT_BASE_PATH='/usr/local/apps/potential'
+PROJECT_BASE_PATH='/usr/local/apps/w88'
 
 git pull
 $PROJECT_BASE_PATH/env/bin/python manage.py migrate
