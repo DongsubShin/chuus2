a42d8ee4d14b67f4668cdf71f5f5377124479f8c
DongsubShin <pottersd2@naver.com>
2023-04-08 07:47:15 +0900

    Deploy

--- deploy/update.sh
+++ deploy/update.sh
@@ -4,6 +4,8 @@ set -e
 
 PROJECT_BASE_PATH='/usr/local/apps/pably'
 
+git pull
+
 $PROJECT_BASE_PATH/env/bin/python manage.py migrate
 $PROJECT_BASE_PATH/env/bin/python manage.py collectstatic --noinput
 supervisorctl restart potential
