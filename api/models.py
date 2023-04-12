d71d1bb001a9a80778f6223656e0b0ce7be9a8e9
DongsubShin <pottersd2@naver.com>
2023-04-12 11:34:36 +0900

    Deploy

--- api/models.py
+++ api/models.py
@@ -3,4 +3,5 @@ from django.db import models
 # Create your models here.
 
 class EarlyBird(models.Model):
-    email = models.EmailField(max_length=255, unique=True)
+    email = models.EmailField(max_length=255, null=True)
+    instagram = models.CharField(max_length=255, null=True, default=None)
