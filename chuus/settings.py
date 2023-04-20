8603c5d6366bb707bfcd557e73b00498921787a5
DongsubShin <pottersd2@naver.com>
2023-04-20 09:49:38 +0900

    Deploy

--- chuus/settings.py
+++ chuus/settings.py
@@ -211,6 +211,9 @@ STATICFILES_DIRS=[
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
 LOGIN_URL = 'login'
 LOGOUT_URL = 'logout'
 
